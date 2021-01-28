b1d223b146174cba94fe982483e3ee6c34fb6a25
Ashish-1699 <51814838+Ashish-1699@users.noreply.github.com>
2021-01-28 17:45:43 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello
 {
     public static void main(String[] args) {
         System.out.println("Welcome !! Happy Coding :)");
+        System.out.println("Apply some changes to check webhook works");
     }
 }
